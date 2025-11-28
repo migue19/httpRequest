9fbb1b11fd814c8e0c12a09f0adcc7166e074625
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2025-11-27 18:13:42 -0600

    Initial Commit

---
+++ HttpRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HttpRequest
+//
+//  Created by Miguel Mexicano Herrera on 27/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
